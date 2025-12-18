b39420a1ca69da1e5cc586fd5a1a504b1d365f4e
Sam Loeffler <samloeffler123@gmail.com>
2025-12-18 05:54:54 -0600

    fixed how query data is passed into the .execute methods

--- write_to_sql.py
+++ write_to_sql.py
@@ -1,32 +1,49 @@
 import sqlite3
 
-def push_author_data(author, db):
-    connection = sqlite3.connect(db)
+DATA_BASE = "users_books.db"
+
+def init_connection_to_sql():
+    connection = sqlite3.connect(DATA_BASE)
     cursor = connection.cursor()
-    if not check_if_author_exists(cursor, author):
-        cursor.execute(f"INSERT INTO authors (author_name) VALUES ('{author}')")
+    return cursor, connection
+
+def push_author_data(author, table_name, column_name):
+    cursor, connection = init_connection_to_sql()
+    for name in author:
+        if not check_if_data_exists_in_table(cursor, table_name, column_name, name):
+            cursor.execute(f"INSERT INTO {table_name} ({column_name}) VALUES (?)", (name,))
+            connection.close()
+            print(f"{author} added to {table_name}")
+            return True
+        else:
+            print("author already in db")
+            connection.close()
+            return False
+
+def push_book_data(book_name, isbn_number, publication_year, format, date_read, table_name, column_name):
+    cursor, connection = init_connection_to_sql()
+    if not check_if_data_exists_in_table(cursor, table_name, column_name, isbn_number):
+        cursor.execute(f"INSERT INTO {table_name} (book_name, isbn, publication_year, format, date_read) VALUES (?, ?, ?, ?, ?)", (book_name, isbn_number, publication_year, format, date_read))
         connection.close()
+        print(f"{book_name} added to {table_name}")
         return True
     else:
         connection.close()
         return False
 
-def check_if_author_exists(cursor, author_name):
-    count = cursor.execute(f"SELECT COUNT(*) FROM authors WHERE author_name = '{author_name}'")
+def push_authors_books_data(author_name, isbn, table_name):
+    cursor, connection = init_connection_to_sql()
+    # get id's
+    author_id = cursor.execute("SELECT author_id FROM authors WHERE author_name = ?", author_name)
+    book_id = cursor.execute("SELECT book_id FROM books WHERE isbn = ?", isbn)
+    author_id_fetch = cursor.fetchone(author_id)
+    book_id_fetch = cursor.fetchone(book_id)
+    cursor.execute(f"INSERT INTO {table_name} (author_id, book_id) VALUES (?, ?)", (author_id_fetch, book_id_fetch))
+    connection.close()
+    print(f"{book_id_fetch} and {author_id_fetch} added to {table_name}")
+
+# data_name is what we are getting from the api query (EX: WHERE isbn = isbn)
+def check_if_data_exists_in_table(cursor, table_name, column_name, data_name):
+    cursor.execute(f"SELECT COUNT(*) FROM {table_name} WHERE {column_name} = ?", (data_name,))
+    count = cursor.fetchone()[0]
     return count
-
-def push_book_data(book_name, isbn_number, publication_year, format, date_read, db):
-    connection = sqlite3.connect(db)
-    cursor = connection.cursor()
-    if not check_if_book_exists(cursor, isbn_number):
-        cursor.execute(f"INSTER INTO books (book_name, isbn, publication_year, format, date_read) VALUES ('{book_name}, {isbn_number}, {publication_year}, {format}, {date_read}')")
-        connection.close()
-        return True
-    else:
-        connection.close()
-        return False
-
-def check_if_book_exists(cursor, isbn_number):
-    count = cursor.execute(f"SELECT COUNT(*) FROM books WHERE isbn = '{isbn_number}'")
-    return count
-
