0499df5314fbfb93e795efd99495ecac5597ced4
Sam Loeffler <samloeffler123@gmail.com>
2025-12-18 05:56:27 -0600

    changed what data to return

--- google_api.py
+++ google_api.py
@@ -1,5 +1,4 @@
 import requests
-from barcode import scan_code
 
 def get_book_data(isbn):
     response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}")
@@ -8,6 +7,4 @@ def get_book_data(isbn):
     author = data["items"][0]["volumeInfo"]["authors"]
     publication_year = data["items"][0]["volumeInfo"]["publishedDate"]
     format = data["items"][0]["volumeInfo"]["printType"]
-    isbn_num = data["items"][0]["volumeInfo"]["industryIdentifiers"][0]["identifier"]
-    
-get_book_data(scan_code())
+    return title, author, publication_year, format
