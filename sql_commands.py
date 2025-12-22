bb6b9d24cd9639da8d4ac82124e583f7605796ec
Sam Loeffler <samloeffler123@gmail.com>
2025-12-22 06:44:02 -0600

    created filter_data func to search data provided

--- sql_commands.py
+++ sql_commands.py
@@ -21,6 +21,10 @@ BOOK_CATEGORIES_COLUMN_NAME = "categories"
 def init_connection_to_sql():
     return sqlite3.connect(DATA_BASE)
 
+def commit_and_close_connection(connection_to_db):
+    connection_to_db.commit()
+    connection_to_db.close()
+
 def add_book_manually():
     connection = init_connection_to_sql()
     book_name = input("Book name: \n")
@@ -43,31 +47,24 @@ def add_read_date_to_book():
     commit_and_close_connection(connection)
 
 def add_column_to_table(table_name, new_column_name, connection_to_db):
-    # ALTER TABLE table ADD COLUMN new_column;
     cursor = connection_to_db.cursor()
     cursor.execute(f"ALTER TABLE {table_name} ADD COLUMN {new_column_name}")
     commit_and_close_connection(connection_to_db)
 
 def update_data_in_table(table_name, column_name, data_name_to_replace, new_data_name, connection_to_db):
-    # UPDATE table_name SET column_1 = value_1, column_2 = value2
     cursor = connection_to_db.cursor()
     cursor.execute(f"UPDATE {table_name} SET {column_name} = ? WHERE {column_name} = ?", (new_data_name, data_name_to_replace,))
     commit_and_close_connection(connection_to_db)
-    
+
 def remove_data_from_table(table_name, column_name, data_name, connection_to_db):
-    # DELETE FROM table WHERE column_name = data;
     cursor = connection_to_db.cursor()
     user_response = input(f"Are you sure you want to delete {data_name} from {table_name} in {column_name}? : (Y or N) ")
     if user_response:
         cursor.execute(f"DELETE FROM {table_name} WHERE {column_name} = ?", (data_name,))
         commit_and_close_connection(connection_to_db)
 
-def filter_by_category():
-    pass
-
-def filter_by_author():
-    pass
-
-def commit_and_close_connection(connection_to_db):
-    connection_to_db.commit()
-    connection_to_db.close()
+def filter_data(table_name, column_names: tuple, connection_to_db, data_to_filter, column_name):
+    cursor = connection_to_db.cursor()
+    cursor.execute(f"SELECT {column_names} FROM {table_name} WHERE {column_name} = ?", data_to_filter)
+    data = cursor.fetchall()
+    print(data)
