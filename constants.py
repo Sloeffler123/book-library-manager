2294e46cf54576df8d8c1c19ed1ede6a07b07742
Sam Loeffler <samloeffler123@gmail.com>
2025-12-29 06:27:34 -0600

    made new folder to hold all constants

---
+++ constants.py
@@ -0,0 +1,21 @@
+DATA_BASE = "users_books.db"
+
+#Author table
+AUTHOR_TABLE_NAME = "authors"
+AUTHOR_NAME_COLUMN = "author_name"
+AUTHOR_ID_COLUMN_NAME = "author_id"
+
+#Authors_books table
+AUTHOR_BOOKS_TABLE_NAME = "authors_books"
+AUTHOR_BOOKS_AUTHOR_ID = "author_id"
+AUTHOR_BOOKS_BOOK_ID = "book_id"
+
+#Book table
+BOOK_TABLE_NAME = "books"
+BOOK_NAME_COLUMN = "book_name"
+BOOK_ISBN_COLUMN = "isbn"
+BOOK_ID_COLUMN_NAME = "book_id"
+BOOK_PUBLICATION_YEAR_COLUMN_NAME = "publication_year"
+BOOK_FORMAT_COLUMN_NAME = "format"
+BOOK_DATE_READ_COLUMN_NAME = "date_read"
+BOOK_CATEGORIES_COLUMN_NAME = "categories"
