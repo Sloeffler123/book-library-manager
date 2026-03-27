fe208dd1f32ebbe5a1c9ab883f9f9ce67a4df439
Sam Loeffler <samloeffler123@gmail.com>
2026-03-27 06:23:02 -0500

    removed constants from main

--- main.py
+++ main.py
@@ -2,21 +2,12 @@ from sql_commands import init_connection_to_sql
 from write_to_sql import push_author_data, push_book_data, push_authors_books_data
 from barcode import scan_code
 from google_api import get_book_data
-from constants import AUTHOR_TABLE_NAME, AUTHOR_NAME_COLUMN, BOOK_TABLE_NAME, BOOK_NAME_COLUMN, BOOK_ISBN_COLUMN, BOOK_PUBLICATION_YEAR_COLUMN_NAME, BOOK_FORMAT_COLUMN_NAME, BOOK_DATE_READ_COLUMN_NAME, BOOK_CATEGORIES_COLUMN_NAME, AUTHOR_BOOKS_TABLE_NAME, AUTHOR_ID_COLUMN_NAME, BOOK_ID_COLUMN_NAME
-
 
 def main():
     connection = init_connection_to_sql()
     isbn_13 = scan_code()
     book_name, author, publication_year, format, categories = get_book_data(isbn_13)
-    push_author_data(author, AUTHOR_TABLE_NAME, AUTHOR_NAME_COLUMN, connection)
-    push_book_data(book_name, isbn_13, publication_year, format, "NULL", categories, BOOK_TABLE_NAME, BOOK_NAME_COLUMN, BOOK_ISBN_COLUMN, BOOK_PUBLICATION_YEAR_COLUMN_NAME, BOOK_FORMAT_COLUMN_NAME, BOOK_DATE_READ_COLUMN_NAME, BOOK_CATEGORIES_COLUMN_NAME, connection)
-    push_authors_books_data(author, isbn_13, AUTHOR_BOOKS_TABLE_NAME, AUTHOR_NAME_COLUMN, AUTHOR_ID_COLUMN_NAME, BOOK_ISBN_COLUMN, BOOK_ID_COLUMN_NAME, AUTHOR_TABLE_NAME, BOOK_TABLE_NAME, connection)
+    push_author_data(author, connection)
+    push_book_data(book_name, isbn_13, publication_year, format, "NULL", categories, connection)
+    push_authors_books_data(author, isbn_13, connection)
     connection.close()
-main()
-
-# make testing
-# export to a csv file
-# fix crud commands
-# sort books by algorithm
-# use plotly to see when i read book
