70749702e55ff49f9669d59f739f91900a1ee2a3
Sam Loeffler <samloeffler123@gmail.com>
2026-03-27 06:22:16 -0500

    changed the testing files to match the changes to write_to_sql and sql_commands

--- testing/sql_commands_test.py
+++ testing/sql_commands_test.py
@@ -4,7 +4,7 @@ from google_api import get_book_data
 from write_to_sql import push_author_data, push_authors_books_data, push_book_data
 from pathlib import Path
 from sql_commands import add_book_manually, add_read_date_to_book, add_column_to_table, update_data_in_table, remove_data_from_table, filter_data_by_book_name_and_author
-from constants import AUTHOR_BOOKS_TABLE_NAME, AUTHOR_ID_COLUMN_NAME, AUTHOR_NAME_COLUMN, AUTHOR_TABLE_NAME, BOOK_CATEGORIES_COLUMN_NAME, BOOK_DATE_READ_COLUMN_NAME, BOOK_FORMAT_COLUMN_NAME, BOOK_ID_COLUMN_NAME, BOOK_ISBN_COLUMN, BOOK_NAME_COLUMN, BOOK_PUBLICATION_YEAR_COLUMN_NAME, BOOK_TABLE_NAME, AUTHOR_BOOKS_AUTHOR_ID, AUTHOR_BOOKS_BOOK_ID
+from constants import AUTHOR_BOOKS_TABLE_NAME, AUTHOR_NAME_COLUMN, AUTHOR_TABLE_NAME, BOOK_DATE_READ_COLUMN_NAME, BOOK_NAME_COLUMN, BOOK_TABLE_NAME
 
 SCHEMA_PATH = Path(__file__).parent.parent / "schema.sql"
 
@@ -53,7 +53,7 @@ def test_add_read_date_to_book(db_connection):
     add_book_data_helper(db_connection)
     cursor = db_connection.cursor()
     add_read_date_to_book(db_connection)
-    cursor.execute(f"SELECT {BOOK_DATE_READ_COLUMN_NAME} FROM books")
+    cursor.execute(f"SELECT {BOOK_DATE_READ_COLUMN_NAME} FROM {BOOK_TABLE_NAME}")
     result = cursor.fetchone()
     assert result[0] == data[0]
 
@@ -61,7 +61,7 @@ def test_add_column_to_table(db_connection):
     add_book_data_helper(db_connection)
     cursor = db_connection.cursor()
     new_table = ["Rating"]
-    add_column_to_table(BOOK_TABLE_NAME, new_table[0], db_connection)
+    add_column_to_table(new_table[0], db_connection)
     cursor.execute("SELECT name FROM pragma_table_info('books') WHERE name = 'Rating'")
     result = cursor.fetchone()
     assert result[0] == new_table[0]
@@ -87,11 +87,11 @@ def test_remove_data_from_table(db_connection):
 def test_filter_data(db_connection):
     add_book_data_helper(db_connection)
     data = ("Angels and Demons", "Dan Brown")
-    filtered_data = filter_data_by_book_name_and_author(BOOK_TABLE_NAME, AUTHOR_TABLE_NAME, BOOK_NAME_COLUMN, AUTHOR_NAME_COLUMN, AUTHOR_BOOKS_TABLE_NAME, BOOK_ID_COLUMN_NAME, AUTHOR_ID_COLUMN_NAME, AUTHOR_BOOKS_BOOK_ID, AUTHOR_BOOKS_AUTHOR_ID, db_connection)
+    filtered_data = filter_data_by_book_name_and_author(db_connection)
     assert filtered_data[0] == data
 
 def add_book_data_helper(db_connection):
     book_name, author, publication_year, format, categories = get_book_data("9780552150736")
-    push_author_data(author, AUTHOR_TABLE_NAME, AUTHOR_NAME_COLUMN, db_connection)
-    push_book_data(book_name, "9780552150736", publication_year, format, "NULL", categories, BOOK_TABLE_NAME, BOOK_NAME_COLUMN, BOOK_ISBN_COLUMN, BOOK_PUBLICATION_YEAR_COLUMN_NAME, BOOK_FORMAT_COLUMN_NAME, BOOK_DATE_READ_COLUMN_NAME, BOOK_CATEGORIES_COLUMN_NAME, db_connection)
-    push_authors_books_data(author, "9780552150736", AUTHOR_BOOKS_TABLE_NAME, AUTHOR_NAME_COLUMN, AUTHOR_ID_COLUMN_NAME, BOOK_ISBN_COLUMN, BOOK_ID_COLUMN_NAME, AUTHOR_TABLE_NAME, BOOK_TABLE_NAME, db_connection)
+    push_author_data(author, db_connection)
+    push_book_data(book_name, "9780552150736", publication_year, format, "NULL", categories, db_connection)
+    push_authors_books_data(author, "9780552150736", db_connection)
--- testing/write_to_sql_test.py
+++ testing/write_to_sql_test.py
@@ -2,7 +2,7 @@ import pytest
 import sqlite3
 from pathlib import Path
 from write_to_sql import push_author_data, push_book_data, push_authors_books_data
-from constants import AUTHOR_BOOKS_TABLE_NAME, AUTHOR_ID_COLUMN_NAME, AUTHOR_NAME_COLUMN, AUTHOR_TABLE_NAME, BOOK_CATEGORIES_COLUMN_NAME, BOOK_DATE_READ_COLUMN_NAME, BOOK_FORMAT_COLUMN_NAME, BOOK_ID_COLUMN_NAME, BOOK_ISBN_COLUMN, BOOK_NAME_COLUMN, BOOK_PUBLICATION_YEAR_COLUMN_NAME, BOOK_TABLE_NAME
+from constants import AUTHOR_TABLE_NAME, AUTHOR_BOOKS_TABLE_NAME, BOOK_TABLE_NAME, AUTHOR_NAME_COLUMN
 
 SCHEMA_PATH = Path(__file__).parent.parent / "schema.sql"
 
@@ -16,25 +16,25 @@ def db_connection():
 
 def test_push_author_data(db_connection):
     cursor = db_connection.cursor()
-    push_author_data(["Tom Clancy"], AUTHOR_TABLE_NAME, AUTHOR_NAME_COLUMN, db_connection)
-    cursor.execute(f"SELECT author_name FROM authors WHERE author_name = ?", ("Tom Clancy",))
+    push_author_data(["Tom Clancy"], db_connection)
+    cursor.execute(f"SELECT {AUTHOR_NAME_COLUMN} FROM {AUTHOR_TABLE_NAME} WHERE {AUTHOR_NAME_COLUMN} = ?", ("Tom Clancy",))
     result = cursor.fetchone()
     assert result[0] == "Tom Clancy"
 
 def test_push_book_data(db_connection):
     cursor = db_connection.cursor()
     data = [(1, "Patriot Games", "9780425134351", "1992-05-01", "BOOK", "NULL", "Fiction")]
-    push_book_data(data[0][1], data[0][2], data[0][3], data[0][4], data[0][5], data[0][6], BOOK_TABLE_NAME, BOOK_NAME_COLUMN, BOOK_ISBN_COLUMN, BOOK_PUBLICATION_YEAR_COLUMN_NAME, BOOK_FORMAT_COLUMN_NAME, BOOK_DATE_READ_COLUMN_NAME, BOOK_CATEGORIES_COLUMN_NAME, db_connection)
-    cursor.execute(f"SELECT * FROM books")
+    push_book_data(data[0][1], data[0][2], data[0][3], data[0][4], data[0][5], data[0][6], db_connection)
+    cursor.execute(f"SELECT * FROM {BOOK_TABLE_NAME}")
     result = cursor.fetchall()
     assert result == data
 
 def test_push_authors_books_data(db_connection):
     cursor = db_connection.cursor()
     data = [(1, "Patriot Games", "9780425134351", "1992-05-01", "BOOK", "NULL", "Fiction")]
-    push_author_data(["Tom Clancy"], AUTHOR_TABLE_NAME, AUTHOR_NAME_COLUMN, db_connection)
-    push_book_data(data[0][1], data[0][2], data[0][3], data[0][4], data[0][5], data[0][6], BOOK_TABLE_NAME, BOOK_NAME_COLUMN, BOOK_ISBN_COLUMN, BOOK_PUBLICATION_YEAR_COLUMN_NAME, BOOK_FORMAT_COLUMN_NAME, BOOK_DATE_READ_COLUMN_NAME, BOOK_CATEGORIES_COLUMN_NAME, db_connection)
-    push_authors_books_data(["Tom Clancy"], "9780425134351", AUTHOR_BOOKS_TABLE_NAME, AUTHOR_NAME_COLUMN, AUTHOR_ID_COLUMN_NAME, BOOK_ISBN_COLUMN, BOOK_ID_COLUMN_NAME, AUTHOR_TABLE_NAME, BOOK_TABLE_NAME, db_connection)
-    cursor.execute(f"SELECT * FROM authors_books")
+    push_author_data(["Tom Clancy"], db_connection)
+    push_book_data(data[0][1], data[0][2], data[0][3], data[0][4], data[0][5], data[0][6], db_connection)
+    push_authors_books_data(["Tom Clancy"], "9780425134351", db_connection)
+    cursor.execute(f"SELECT * FROM {AUTHOR_BOOKS_TABLE_NAME}")
     result = cursor.fetchall()
     assert result == [(1, 1)]
