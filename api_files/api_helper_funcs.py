d9b5fd1cfc550173474ebeb25b2c1b47ef1d9c2c
Sam <samloeffler123@gmail.com>
2026-09-12 08:09:54 -0500

    changed how counter var in api_helper_funcs is tracked

--- api_files/api_helper_funcs.py
+++ api_files/api_helper_funcs.py
@@ -5,12 +5,10 @@ def get_main_page_data(conn) -> dict:
     statement = filter_title_author_review_date_read(conn)
     lst = ["title", "author", "review", "date_read"]
     main_dict = {}
-    counter = 0
-    for row in statement:
+    for counter, row in enumerate(statement):
         row_dict = {}
         for index, data in enumerate(row):
             row_dict[lst[index]] = data
-        counter += 1
         main_dict[str(counter)] = row_dict
     conn.close()
     return main_dict
